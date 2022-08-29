f3f646e0647bea8d93981a8c9817a0d794b98307
doctype-Melvin <doctype.melvin@gmail.com>
2022-08-29 14:44:03 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const input = document.querySelectorAll('input')
+
+// input.forEach(field => field.addEventListener('input', () => {
+//     field.setCustomValidity('')
+//     field.checkValidity()
+// }))
+
+// input.forEach(field => field.addEventListener('invalid', () => {
+//     if (field.value === '') {
+//         field.setCustomValidity(`Please enter a ${field.type}`)
+//     } else {
+//         field.setCustomValidity('This is not the correct input type')
+//     }
+// }))
+
+// input.forEach(field => field.addEventListener('input', (e) => {
+//     if (field.validity.typeMismatch) {
+//         field.setCustomValidity('Something went wrong')
+//         field.reportValidity()
+//     }else {
+//         field.setCustomValidity('')
+//     }
+// }))
+
+const phone = document.getElementById('phone')
+
+phone.addEventListener('input', () => {
+    phone.setCustomValidity('')
+    phone.checkValidity()
+})
+phone.addEventListener('invalid', () => {
+    if (phone.value === ''){
+        phone.setCustomValidity('This field is optional')
+    }else {
+        phone.setCustomValidity('I am expecting a phone number')
+    }
+})
