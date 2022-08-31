0dd1c4a95f2b87884944610ef26bb7f0367ad04f
doctype-Melvin <doctype.melvin@gmail.com>
2022-08-31 12:10:45 +0200

    input borders turn red on focus before entering valid data

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ const form = document.querySelector('form')
 form.reset()
 
 inputs.forEach(field => field.addEventListener('input', () => {
-    if(!field.checkValidity()) {
+    if(!field.checkValidity() || field.value == '') {
         field.classList.remove('success')
         field.classList.add('fail')
         field.parentElement.lastChild.previousSibling.textContent = `Please enter valid ${field.name}`
-        
     }else{
         field.classList.remove('fail')
         field.classList.add('success')
