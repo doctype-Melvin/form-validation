995209cccd38ffa9db7e69435a167abb672971b9
doctype-Melvin <doctype.melvin@gmail.com>
2022-08-31 15:21:54 +0200

    adds README

--- main.js
+++ main.js
@@ -2,47 +2,64 @@ const inputs = document.querySelectorAll('.input')
 const form = document.querySelector('form')
 form.reset()
 
+const username = document.getElementById('username')
 const password = document.getElementById('pwd')
 const passwordConfirm = document.getElementById('pwdConfirm')
 
 inputs.forEach(field => field.addEventListener('input', () => { //
     if(!field.checkValidity() || field.value == '') {
-        field.classList.remove('success')
-        field.classList.add('fail')
-        field.parentElement.lastChild.previousSibling.textContent = `Please enter valid ${field.name}`
+        validator(false, field)
     }else{
-        field.classList.remove('fail')
-        field.classList.add('success')
+        validator(true, field)
     }
 }))
 
 document.querySelector('.btn').addEventListener('click', (e) => {
-    if(!form.checkValidity()) {
-        e.preventDefault
+    if(password.value != passwordConfirm.value) {
+        e.preventDefault()
     }
+   
 })
 
 password.addEventListener('input', () => {
-    const passRegEx = /^(?=.*[0-9])(?=.*[!@#$^&*])[a-zA-Z0-9!@#$^&*]{5,15}$/
+    const passRegEx = /^(?=.*[0-9])(?=.*[!@#$^&*])[a-zA-Z0-9!@#$^&*]{5,20}$/
     if(!password.value.match(passRegEx)){
-        password.classList.remove('success')
-        password.classList.add('fail')
-        password.parentElement.lastChild.previousSibling.textContent = 'Please enter valid password'
+        validator(false, password)
     }else{
-        password.classList.remove('fail')
-        password.classList.add('success')
+        validator(true, password)
     }
+})
+
+password.addEventListener('change', () => {
+    if(password.value !== passwordConfirm.value || 
+        password.value.length !== passwordConfirm.value.length) {
+            validator(false, passwordConfirm)
+        }else{
+            validator(true, password)
+        }
 })
 
 passwordConfirm.addEventListener('input', () => {
     const passRegEx = password.value
     console.log(passRegEx)
     if(!passwordConfirm.value.match(passRegEx) || passwordConfirm.value.length !== password.value.length){
-        passwordConfirm.classList.remove('success')
-        passwordConfirm.classList.add('fail')
-        passwordConfirm.parentElement.lastChild.previousSibling.textContent = 'Passwords do not match'
+        validator(false, passwordConfirm)
     }else if (passwordConfirm.value.length === password.value.length){
-        passwordConfirm.classList.remove('fail')
-        passwordConfirm.classList.add('success')
+        validator(true, password)
     }
 })
+
+function validator (bool, field) {
+    if(!bool && field.name != 'pwdConfirm') {
+        field.classList.remove('success')
+        field.classList.add('fail')
+        field.parentElement.lastChild.previousSibling.textContent = `Please enter valid ${field.type}`
+    }else if (!bool && field.name == 'pwdConfirm') {
+        field.classList.remove('success')
+        field.classList.add('fail')
+        field.parentElement.lastChild.previousSibling.textContent = `Passwords don't match`
+    }else {
+        field.classList.remove('fail')
+        field.classList.add('success')
+    }
+}
