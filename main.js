09619a07a2e2533a4711b43e77d349b9c4be9cb7
doctype-Melvin <doctype.melvin@gmail.com>
2022-08-31 11:30:42 +0200

    shows error message with required data type

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const btn = document.querySelector('.btn')
-const userInput = document.getElementById('username')
-const error = document.querySelector('.errMessage')
 const inputs = document.querySelectorAll('.input')
 const form = document.querySelector('form')
 form.reset()
@@ -9,32 +6,16 @@ inputs.forEach(field => field.addEventListener('input', () => {
     if(!field.checkValidity()) {
         field.classList.remove('success')
         field.classList.add('fail')
-        error.textContent = `Please enter valid ${field.name}`
+        field.parentElement.lastChild.previousSibling.textContent = `Please enter valid ${field.name}`
+        
     }else{
         field.classList.remove('fail')
         field.classList.add('success')
     }
 }))
 
-// function validation(field) {
-//     if(!field.checkValidity()) {
-//         document.querySelector('.error').style.visibility = 'visible'
-//         document.querySelector('.check').style.visibility = 'hidden'
-//         error.style.visibility = 'visible'
-//         error.textContent = `Please enter valid`
-//         console.log(`${field.name} is invalid`)
-//     }else{
-//         error.style.visibility = 'hidden'
-//         document.querySelector('.error').style.visibility = 'hidden'
-//         document.querySelector('.check').style.visibility = 'visible'
-//         console.log(`${field.name} is valid`)
-//     }
-// }
-// inputs.forEach(field => field.addEventListener('input', () => {
-//     let inputField = document.getElementById(`${field.id}`)
-//     console.log(inputField)
-//     validation(inputField)
-// }))
-// userInput.addEventListener('input', () => {
-//     validation(userInput)
-// })
+document.querySelector('.btn').addEventListener('click', (e) => {
+    if(!form.checkValidity()) {
+        e.preventDefault
+    }
+})
