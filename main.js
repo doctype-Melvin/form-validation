bf4d6b0dcd85dad539410e314b2f177f3be75262
doctype-Melvin <doctype.melvin@gmail.com>
2022-08-31 13:37:21 +0200

    adds more form controls and validates password

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const inputs = document.querySelectorAll('.input')
 const form = document.querySelector('form')
 form.reset()
 
-inputs.forEach(field => field.addEventListener('input', () => {
+const password = document.getElementById('pwd')
+const passwordConfirm = document.getElementById('pwdConfirm')
+
+inputs.forEach(field => field.addEventListener('input', () => { //
     if(!field.checkValidity() || field.value == '') {
         field.classList.remove('success')
         field.classList.add('fail')
@@ -16,5 +19,30 @@ inputs.forEach(field => field.addEventListener('input', () => {
 document.querySelector('.btn').addEventListener('click', (e) => {
     if(!form.checkValidity()) {
         e.preventDefault
+    }
+})
+
+password.addEventListener('input', () => {
+    const passRegEx = /^(?=.*[0-9])(?=.*[!@#$^&*])[a-zA-Z0-9!@#$^&*]{5,15}$/
+    if(!password.value.match(passRegEx)){
+        password.classList.remove('success')
+        password.classList.add('fail')
+        password.parentElement.lastChild.previousSibling.textContent = 'Please enter valid password'
+    }else{
+        password.classList.remove('fail')
+        password.classList.add('success')
+    }
+})
+
+passwordConfirm.addEventListener('input', () => {
+    const passRegEx = password.value
+    console.log(passRegEx)
+    if(!passwordConfirm.value.match(passRegEx) || passwordConfirm.value.length !== password.value.length){
+        passwordConfirm.classList.remove('success')
+        passwordConfirm.classList.add('fail')
+        passwordConfirm.parentElement.lastChild.previousSibling.textContent = 'Passwords do not match'
+    }else if (passwordConfirm.value.length === password.value.length){
+        passwordConfirm.classList.remove('fail')
+        passwordConfirm.classList.add('success')
     }
 })
